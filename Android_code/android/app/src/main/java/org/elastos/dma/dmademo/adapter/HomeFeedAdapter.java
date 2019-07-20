cc42d5fcb04525e8ffeef3ac0e3a51c015f47b52
Chen Jensen <chenjensen@Chens-MacBook-Pro.local>
2019-07-21 03:05:48 +0800

    Profile update

--- Android_code/android/app/src/main/java/org/elastos/dma/dmademo/adapter/HomeFeedAdapter.java
+++ Android_code/android/app/src/main/java/org/elastos/dma/dmademo/adapter/HomeFeedAdapter.java
@@ -21,6 +21,7 @@ public class HomeFeedAdapter extends RecyclerView.Adapter<HomeFeedAdapter.HomeFe
 
     private List<Game> mGames = new ArrayList<>();
     private Context mContext;
+    private int[] mImages = {R.mipmap.bg_liu, R.mipmap.bg_meizhou, R.mipmap.bg_nanluo, R.mipmap.bg_shaghai, R.mipmap.bg_wuhun};
 
     public HomeFeedAdapter(Context context) {
         mContext = context;
@@ -41,6 +42,7 @@ public class HomeFeedAdapter extends RecyclerView.Adapter<HomeFeedAdapter.HomeFe
     @Override
     public void onBindViewHolder(@NonNull HomeFeedAdapter.HomeFeedViewHolder viewHolder, int i) {
         final Game game = mGames.get(i);
+        viewHolder.cover.setImageDrawable(mContext.getDrawable(mImages[i % mImages.length]));
 //        viewHolder.cover.setBackground(R.drawable.bg_alert_b);
         viewHolder.name.setText(game.getName());
         viewHolder.content.setText(game.getProduction());
