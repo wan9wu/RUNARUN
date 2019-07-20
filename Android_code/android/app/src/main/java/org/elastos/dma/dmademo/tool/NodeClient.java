1c49383ba022a8c675a16f31097da53feb812f11
xianxianzhou <xyyz1207@163.com>
2019-07-21 00:10:13 +0800

    发起活动

--- Android_code/android/app/src/main/java/org/elastos/dma/dmademo/tool/NodeClient.java
+++ Android_code/android/app/src/main/java/org/elastos/dma/dmademo/tool/NodeClient.java
@@ -6,8 +6,11 @@ import org.elastos.dma.service.merchant.ETHMerchantService;
 import org.elastos.dma.service.merchant.impl.ETHMerchantServiceImpl;
 import org.elastos.dma.service.passport.PassportService;
 import org.elastos.dma.service.passport.impl.PassportServiceImpl;
+import org.elastos.dma.service.storage.ipfs.IpfsService;
 import org.elastos.dma.service.wallet.ElaWalletService;
 import org.elastos.dma.service.wallet.EthWalletService;
+
+import java.math.BigInteger;
 
 /**
  * Created by xianxian on 2018/3/20.
@@ -16,11 +19,15 @@ import org.elastos.dma.service.wallet.EthWalletService;
 public class NodeClient {
     public static String didip = "http://54.64.220.165:21604";//测试 http://did-mainnet-node-lb-1452309420.ap-northeast-1.elb.amazonaws.com:20604
     public static String elaip = "http://54.64.220.165:21334";
-    public static String ip = "http://47.105.136.158:8545";//DMAtestlink
+//    public static String ip = "http://47.105.136.158:8545";//DMAtestlink
+    public static String ip = "http://52.205.30.16:8545";//ETH侧链
     public static String ipfsIP="http://47.105.136.158:8080";//ipfs生产
+    public static BigInteger gasPrice= BigInteger.valueOf(20000000000l);
+    public static  BigInteger gasLimit= BigInteger.valueOf(6602513);
     private volatile static EthWalletService ethWalletService;
     private volatile static PassportService passportService;
     private volatile static ElaWalletService elaWalletService;
+    private volatile static IpfsService ipfsService;
     private volatile static AccountService accountService;
     private volatile  static ETHMerchantService ethMerchantService;
 
@@ -58,6 +65,12 @@ public class NodeClient {
             ethMerchantService = new ETHMerchantServiceImpl(ip );
         }
         return ethMerchantService;
+    }
+    public static IpfsService getIpfsServive() {
+        if (ipfsService == null) {
+            ipfsService = IpfsService.getInstance("/ip4/47.105.136.158/tcp/5001", ipfsIP);
+        }
+        return ipfsService;
     }
 
 
